15b703184eb2db7479895f1c1ee3cb7e4b2e660f
apratimkho06 <apratim.khobragade@gmail.com>
2019-07-12 10:50:31 +0530

    Initial commit

---
+++ app/src/main/java/com/example/moviedatabase/MoviesAdapter.java
@@ -0,0 +1,32 @@
+package com.example.moviedatabase;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MovieViewHolder> {
+
+    @Override
+    public MovieViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_movie,parent,false);
+        return new MovieViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MovieViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 50;
+    }
+
+    public class MovieViewHolder extends RecyclerView.ViewHolder {
+        public MovieViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+}
